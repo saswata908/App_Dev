db64e82aaa14a23972d28b3959073d9d908170a2
saswata908 <157130478+saswata908@users.noreply.github.com>
2025-05-18 21:50:27 +0530

    Delete lib/main.dart

--- lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:first_app/gradient_container.dart';
-
-void main() {
-  runApp(const MaterialApp(home: Scaffold(body: GradientContainer.pre())));
-}
